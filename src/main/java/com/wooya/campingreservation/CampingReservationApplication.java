6379c72ee6443f24cc139fdbc623bd9000f368fa
= <=>
2023-05-18 23:09:22 +0900

    init

---
+++ src/main/java/com/wooya/campingreservation/CampingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.wooya.campingreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wooya/campingreservation/CampingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooya.campingreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
